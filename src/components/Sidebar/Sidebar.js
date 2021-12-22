99026dfccbd259d9beb2f23de90cca100fc2cd70
Amin Arshadinia <a.arshadinia@novin.dev>
2021-12-22 13:00:09 +0330

    feat : Sidebar Icons updated

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,14 +4,24 @@ import SidebarRow from './SidebarRow'
 import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
 import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 import MailIcon from '@material-ui/icons/Mail';
+import SaveIcon from '@material-ui/icons/Save';
+import TimelapseIcon from '@material-ui/icons/Timelapse';
+import EventIcon from '@material-ui/icons/Event';
+import EWorkIcon from '@material-ui/icons/Work';
+import {useStateValue} from '../StateProvider/StateProvider'
 
 const Sidebar = () => {
+  const [{user} , dispatch] = useStateValue();
   return (
     <div className='sidebar'>
-    <SidebarRow title='Pages' src={'https://www.w3schools.com/howto/img_avatar.png'} />
-    <SidebarRow title='Friends'  Icon={LocalHospitalIcon}/>
-    <SidebarRow title='Warlock'  Icon={EmojiFlagsIcon}/>
-    <SidebarRow title='Mages'  Icon={MailIcon}/>
+    <SidebarRow src={user.photoURL} title={user.displayName} />
+    <SidebarRow title='Find Friends'  Icon={LocalHospitalIcon}/>
+    <SidebarRow title='Groups'  Icon={EmojiFlagsIcon}/>
+    <SidebarRow title='Watch'  Icon={MailIcon}/>
+    <SidebarRow title='Memories'  Icon={TimelapseIcon}/>
+    <SidebarRow title='Saved'  Icon={SaveIcon}/>
+    <SidebarRow title='Events'  Icon={EventIcon}/>
+    <SidebarRow title='Jobs'  Icon={EWorkIcon}/>
   </div>
   )
 }
