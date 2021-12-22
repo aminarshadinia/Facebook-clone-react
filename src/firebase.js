b61bd178732f08e3a106d67176026458f5c0f21c
Amin Arshadinia <a.arshadinia@novin.dev>
2021-12-22 12:53:23 +0330

    fix ! : firebaseConfig Update due to firebase email account change

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,24 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import "firebase/auth"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBbaKxbqufRCfrwGpQ3sfuBgIifIhTCP1A",
-  authDomain: "facebook-clone-f4994.firebaseapp.com",
-  projectId: "facebook-clone-f4994",
-  storageBucket: "facebook-clone-f4994.appspot.com",
-  messagingSenderId: "593047789391",
-  appId: "1:593047789391:web:11459d7b291b9465542f3a",
-  measurementId: "G-FNW1K23DBJ"
+  apiKey: "AIzaSyDeMJ85hWRTa5bJk_Upd3tC9jwaVHgCO7Y",
+  authDomain: "facebook-clone-e734f.firebaseapp.com",
+  projectId: "facebook-clone-e734f",
+  storageBucket: "facebook-clone-e734f.appspot.com",
+  messagingSenderId: "786003096347",
+  appId: "1:786003096347:web:2e101e78d91073042f19be",
+  measurementId: "G-QNL41GTDVX"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+export { auth, provider };
+export default db;
